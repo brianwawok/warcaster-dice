95f99d5b1220903c90193ac1f542b3e93706f445
Brian Wawok <brian@listingmirror.com>
2020-03-06 17:38:09 -0500

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,10 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+git init
+git add -A
+git commit -m 'deploy'
+
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:brianwawok/warcaster-dice.git master:gh-pages
 
